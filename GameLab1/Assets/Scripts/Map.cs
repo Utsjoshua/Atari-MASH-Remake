7b57684204f8e520602c6775d9f83a9449101caa
Utsjoshua <Joshua.E.Isaraela@student.uts.edu.au>
2024-02-22 18:24:58 +1100

    Soldier and Tree generation added

---
+++ GameLab1/Assets/Scripts/Map.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    public GameObject TreePrefab;
+    public GameObject SoldierPrefab;
+
+    private int TreeAmount;
+    private int SoldierAmount;
+
+    public void SpawnTrees(){
+
+        for (int i = 0; i < TreeAmount; i++){
+            Vector3 randomSpawnPosition = new Vector3(Random.Range(-3, 9), Random.Range(-4, 5), 0);
+            Instantiate(TreePrefab, randomSpawnPosition, Quaternion.identity);
+        }
+
+    }
+
+    public void SpawnSoldiers(){
+
+        for (int i = 0; i < SoldierAmount; i++){
+            Vector3 randomSpawnPosition = new Vector3(Random.Range(-3, 9), Random.Range(-4, 5), 0);
+            Instantiate(SoldierPrefab, randomSpawnPosition, Quaternion.identity);
+        }
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TreeAmount = Random.Range(1,18);
+        SoldierAmount = Random.Range(5,13);
+        SpawnTrees();
+        SpawnSoldiers();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
