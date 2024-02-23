580a5fa7a47ed02c445b303fe3433b0e60a5a472
Utsjoshua <Joshua.E.Isaraela@student.uts.edu.au>
2024-02-23 13:44:56 +1100

    Fixed bug where soldier is spawned on top of tree

--- GameLab1/Assets/Scripts/Map.cs
+++ GameLab1/Assets/Scripts/Map.cs
@@ -10,10 +10,16 @@ public class Map : MonoBehaviour
     private int TreeAmount;
     private int SoldierAmount;
 
+    private Vector3[] TreeList;
+    private Vector3[] SoldierList;
+    private bool CheckSoldierPosition;
+    private bool Same;
+
     public void SpawnTrees(){
 
         for (int i = 0; i < TreeAmount; i++){
             Vector3 randomSpawnPosition = new Vector3(Random.Range(-3, 9), Random.Range(-4, 5), 0);
+            TreeList[i] = randomSpawnPosition;
             Instantiate(TreePrefab, randomSpawnPosition, Quaternion.identity);
         }
 
@@ -22,7 +28,31 @@ public class Map : MonoBehaviour
     public void SpawnSoldiers(){
 
         for (int i = 0; i < SoldierAmount; i++){
+
             Vector3 randomSpawnPosition = new Vector3(Random.Range(-3, 9), Random.Range(-4, 5), 0);
+            CheckSoldierPosition = true;
+            Same = false;
+
+            while (CheckSoldierPosition){
+                Same = false;
+                randomSpawnPosition = new Vector3(Random.Range(-3, 9), Random.Range(-4, 5), 0);
+                foreach (Vector3 tree in TreeList){
+
+                    if (randomSpawnPosition == tree){
+                        Debug.Log("Same");
+                        Same = true;
+                        break;
+                    }
+
+                }
+
+                if (!Same){
+                    CheckSoldierPosition = false;
+                }
+
+            }
+
+            SoldierList[i] = randomSpawnPosition;
             Instantiate(SoldierPrefab, randomSpawnPosition, Quaternion.identity);
         }
         
@@ -37,6 +67,10 @@ public class Map : MonoBehaviour
     {
         TreeAmount = Random.Range(1,18);
         SoldierAmount = Random.Range(5,13);
+
+        TreeList = new Vector3[TreeAmount];
+        SoldierList = new Vector3[SoldierAmount];
+
         SpawnTrees();
         SpawnSoldiers();
     }
